15307303a4be60cf1bbe34a9915e0d9490b7f8f0
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-07-25 23:38:17 +0200

    Array Stuff

--- tests/Assignments/ass-02.js
+++ tests/Assignments/ass-02.js
@@ -1,5 +1,6 @@
+
 const arStr = ["bahar","Ali","Saba","Vida","Sepi","Kave"]
-const arNum = [43,78,5,890,54,213]
+const arNum = [1365,27,5,86,110,213]
 
 //***concat()***
 //combining two arrays
@@ -58,22 +59,117 @@ console.log(arNew2)*/
   // return something
 //})
 
-const arSrtNew = arStr.map(function(naam,indi){
-    return indi + " : " + naam
+// const arSrtNew = arStr.map(function(naam,indi){
+//     return indi + " : " + naam
 
-})
-console.log(arSrtNew)
+// })
+// console.log(arSrtNew)
 
-const arNumT = arNum.map(function (number) {
-    return number/100+" T"    
-}) 
-console.log(arNumT)
-console.log(arNum.map(function (number) {
-    return number/100+" T"    
-}) )
+// const arNumT = arNum.map(function (number) {
+//     return number/100+" T"    
+// }) 
+// console.log(arNumT)
+// console.log(arNum.map(function (number) {
+//     return number/100+" T"    
+// }) )
+// const arNumHlaf = arNum.map(function(number){
+//     return number/2
+// })
+// console.log(arNumHlaf)
 
-//with Arrow Function!
-const sumTwo=(num1,num2) =>  num1+num2
+// const arNumThird = arNum.map(byThree= number=>number/3)
+// console.log(arNumThird)
 
-console.log(sumTwo(5,56))
+// //with Arrow Function!
+// const sumTwo=(num1,num2) =>  num1+num2
+// console.log(sumTwo(5,56))
+
+// **Filter()**
+// a new array , with items of previous array with a condition
+// const arStrA = arStr.filter(names=> names.startsWith("S"))
+// console.log(arStrA)
+// const arStrFour = arStr.filter(names=> names.length>4)
+// console.log(arStrFour)
+// function ifContainE(name){
+//     return name.includes("e")
+// }
+// const arStrWithE = arStr.filter(ifContainE)
+// console.log(arStrWithE)
+
+// const arNumEven= arNum.filter(digit=> digit%2===0)
+// console.log(arNumEven)
+// const arNumOdd= arNum.filter(digit=> digit%2!==0)
+// console.log(arNumOdd)
+
+//**includes**
+// console.log(arStr.includes("Saba"))
+// console.log(arNum.includes(426/2))
+
+//**find()**
+//find the "first item" that meet the condition
+// function firstFouri(name){
+//    return  name.length ===4
+// }
+// console.log( arStr.find(firstFouri) )
+
+// console.log(arNum.find(number=> number%2===0))
+// console.log(arNum.find(number=> number%2!==0))
+
+//**findindex()**
+//find the "first index of item" that meet the condition
+// console.log( arStr.findIndex(firstFouri) )
+
+// console.log(arNum.findIndex(number=> number%2===0))
+// console.log(arNum.findIndex(number=> number%2!==0))
+
+//**some()**
+//check if "at least" one item meet the condition
+// console.log(arNum.find(number=> number%2!==0))
+// console.log(arNum.findIndex(number=> number%2!==0))
+// console.log(arNum.some(number=> number%2!==0))
+
+// function firstFouri(name){
+//    return  name.length ===6
+// }
+// console.log( arStr.some(firstFouri) )
+
+//**every()**
+//check if "ALL" items meet the condition
+// console.log(arNum.find(number=> number>=5))
+// console.log(arNum.some(number=> number>=5))
+// console.log(arNum.every(number=> number>=5))
+
+// function firstFouri(name){
+//    return  name.length ===4
+// }
+// console.log( arStr.some(firstFouri) )
+// console.log( arStr.every(firstFouri) )
+
+//**slice()**
+// array.slice(start, end) //will act on one item before end
+// const a = arStr.slice(1,4)
+// console.log(arStr)
+// console.log(a)
+
+//**splice()**
+// to remove, add or change items of array
+//array.splice(start, deleteCount, item1, item2, ...)
+console.log(arStr)
+console.log(arStr.splice(0,3, "folan", "bahman", "bisar"))
+console.log(arStr)
+
+console.log(arNum)
+arNum.splice(2,1,55,66)
+console.log(arNum)
+
+arNum.splice(2,2,5)
+console.log(arNum)
+
+
+
+
+
+
+
+
 
